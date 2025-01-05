da4d258ae2c279306649771b5c97b454b2346b92
Zayn <Farras.FF6@gmail.com>
2025-01-06 05:56:53 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return response()->json($request->user(), 200);
+    return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('payment')->group(function () {
+Route::prefix('payment')->group(function() {
     Route::resource('image', ControlPaymentImageController::class)->except(['index', 'create', 'edit']);
 });
 
-Route::prefix('auth')->group(function () {
+Route::prefix('auth')->group(function() {
     Route::resource('register', RegisterUserController::class)->only(['store']);
     Route::resource('login', SessionLoginController::class)->except(['index', 'create', 'edit', 'destroy']);
     Route::post('logout', [SessionLoginController::class, 'destroy'])->middleware('auth:sanctum');
