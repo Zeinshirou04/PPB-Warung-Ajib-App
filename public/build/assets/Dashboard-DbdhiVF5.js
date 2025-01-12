415582293b57d621d0619ed22be530097a97ebcc
Zayn <Farras.FF6@gmail.com>
2025-01-13 04:28:35 +0700

    Commit Build

---
+++ public/build/assets/Dashboard-DbdhiVF5.js
@@ -0,0 +1,1 @@
+import{j as t,M as a}from"./app-CZdKJKIw.js";import{A as r}from"./AuthenticatedLayout-f_xj34uQ.js";function i({username:e}){return t.jsx(r,{children:t.jsx(a,{title:"Dashboard"})})}export{i as default};
